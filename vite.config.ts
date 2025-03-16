54c092aea4a9e24d12cb2f7cfa32ac9c28847fbe
Nicholas Jimmy Alden <89759682+Nixx162@users.noreply.github.com>
2025-03-16 16:16:57 +0800

    configure vite plugin for tannstack

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+    react(),
+    tailwindcss()
+  ],
 })
